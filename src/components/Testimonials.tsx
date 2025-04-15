ead1cad071c48e4a69a14c479544abd9e8ea9e9a
sushmasri1906 <sushmasri1906@gmail.com>
2025-04-15 13:07:29 +0530

    testimonial updated

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -5,14 +5,14 @@ import { FiExternalLink } from "react-icons/fi"; // External link icon
 const testimonials = [
 	{
 		name: "Mahesh Devarakonda",
-		image:
-			"https://res.cloudinary.com/dgulr1hgd/image/upload/v1744695457/mahesh_vgalio.jpg",
+		image: "https://res.cloudinary.com/dgulr1hgd/image/upload/v1744695457/mahesh_vgalio.jpg",
 		feedback:
-			"The Rolanplast UI is beautifully crafted with smooth scroll animations using Framer Motion. The integration of Slick Carousel and Email.js adds great interactivity, and the multi-page structure with dynamic content upload makes the experience seamless and modern. The team responded promptly to all my requirements, providing quick solutions and ensuring smooth communication throughout the development process.",
+			"I&apos;m really happy with how the Rolanplast website turned out. The design is clean, smooth, and interactive. The team was quick to respond, made all the changes I asked for, and made the whole process easy and stress-free.",
 		status: "finished",
 		website: "Rolanplast",
 		url: "https://rolanplast.netlify.app/",
-	},
+	}
+	
 
 	{
 		name: "Narayana Swami",
